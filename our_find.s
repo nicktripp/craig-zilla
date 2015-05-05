1057dbfe34f370bfda2e1797c3de0883d3cf2d73
Nick <nick.e.tripp@gmail.com>
2015-05-04 22:03:06 -0500

    added methods to build trie, just have to change find_words to utilize the trie

--- our_find.s
+++ our_find.s
@@ -1,4 +1,14 @@
 .data
+
+# struct trie_t 108 bytes
+# {
+# 	const char *word;  4 bytes
+# 	trie_t *next[26];  104 bytes
+# };
+TRIE:  .word 0
+.space  10800
+
+
 LEXICON:
 small_dict_size: .word 5
 .align 2
@@ -104,6 +114,8 @@ solve_puzzle:
 
         lw      $a1, LEXICON
 
+# build_trie(const char **wordlist, int num_words)
+	jal 	build_trie
         jal     find_words
 
         la      $t0, SOLUTION
@@ -573,6 +585,132 @@ record_word:
 
 	jr	$ra
 
+
+## trie_t *
+## build_trie(const char **wordlist, int num_words) {
+##     trie_t *root = alloc_trie();
+##
+##     for (int i = 0 ; i < num_words ; i ++) {
+##         // start at first letter of each word
+##         add_word_to_trie(root, wordlist[i], 0);
+##     }
+##
+##     return root;
+## }
+
+build_trie:
+	sub	$sp, $sp, 16
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
+	sw	$s2, 12($sp)
+	move	$s0, $a0		# wordlist
+
+	mul	$t0, $a1, 4		# num_words * 4
+	add	$s1, $s0, $t0		# &wordlist[num_words]
+	jal	alloc_trie
+	move	$s2, $v0		# root
+
+ bt_loop:
+	beq	$s0, $s1, bt_done	# loop till end of array
+	move	$a0, $s2		# root
+	lw	$a1, 0($s0)		# wordlist[i]
+	li	$a2, 0
+	jal	add_word_to_trie
+	add	$s0, $s0, 4		# next word
+	j	bt_loop
+
+ bt_done:
+	move	$v0, $s2		# root
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	lw	$s2, 12($sp)
+	add	$sp, $sp, 16
+	jr	$ra
+
+## void
+## add_word_to_trie(trie_t *trie, const char *word, int index) {
+##     char c = word[index];
+##     if (c == 0) {
+##         trie->word = word;
+##         return;
+##     }
+##
+##     if (trie->next[c - 'A'] == NULL) {
+##         trie->next[c - 'A'] = alloc_trie();
+##     }
+##     add_word_to_trie(trie->next[c - 'A'], word, index + 1);
+## }
+
+add_word_to_trie:
+	sub	$sp, $sp, 16
+	sw	$ra, 0($sp)
+	sw	$s0, 4($sp)
+	sw	$s1, 8($sp)
+	sw	$s2, 12($sp)
+	move	$s0, $a1		# word
+	move	$s1, $a2		# index
+
+	add	$t0, $s0, $s1		# &word[index]
+	lbu	$t0, 0($t0)		# c = word[index]
+	bne	$t0, 0, awtt_search	# !(c == 0)
+	sw	$s0, 0($a0)		# trie->word = word
+	j	awtt_return
+
+ awtt_search:
+	sub	$t0, $t0, 'A'		# c - 'A'
+	mul	$t0, $t0, 4		# (c - 'A') * 4
+	add	$s2, $a0, $t0		# &trie->next[c - 'A'] - 4
+	lw	$v0, 4($s2)		# trie->next[c - 'A']
+	bne	$v0, 0, awtt_skip	# trie->next[c - 'A'] != NULL
+	jal	alloc_trie
+	sw	$v0, 4($s2)		# trie->next[c - 'A'] = alloc_trie()
+
+ awtt_skip:
+	move	$a0, $v0		# trie->next[c - 'A']
+	move	$a1, $s0		# word
+	add	$a2, $s1, 1		# index + 1
+	jal	add_word_to_trie
+
+ awtt_return:
+	lw	$ra, 0($sp)
+	lw	$s0, 4($sp)
+	lw	$s1, 8($sp)
+	lw	$s2, 12($sp)
+	add	$sp, $sp, 16
+	jr	$ra
+
+# alloc trie ###########################################################
+#
+# no arguments
+# returns       the allocated trie node
+
+alloc_trie:
+
+	la 	$t0, TRIE
+	lw 	$t1, 0($t0) 		# t0 = numNodes
+
+	mul	$t2, $t1, 108 		# t2 = 108*numNodes
+
+	add 	$t1, $t1, 1
+	sw 	$t1, 0($t0) 		# t0 = numNodes
+
+	add 	$t0, $t0, 4
+	add 	$v0, $t0, $t2		# t3 = &trieNode[numNodes]
+
+
+	sw	$zero, 0($v0)		# ret_val->word = NULL
+	li	$t0, 0			# i = 0
+
+ at_loop:
+	mul	$t1, $t0, 4		# i * 4
+	add	$t1, $v0, $t1		# &ret_val->next[i] - 4
+	sw	$zero, 4($t1)		# ret_val->next[i] = NULL
+	add	$t0, $t0, 1		# i ++
+	blt	$t0, 26, at_loop	# i < 26
+
+	jr	$ra
 
 
 
